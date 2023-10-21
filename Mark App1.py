d2dfd46bfe28d5ac1b091a196edda82815dd4f43
sivabharathiui <148601605+sivabharathiui@users.noreply.github.com>
2023-10-20 20:32:35 -0700

    project1
    project1

---
+++ Mark App1.py
@@ -0,0 +1,10 @@
+Name="sivabharathi"
+plac="salem"
+Tamil=80
+English=99
+mas=85
+phy=77
+che=90
+computer=95
+Totel=Tamil+English+mas+phy+che+computer
+print("Thish is",Name,plac,"my Tamil mark",Tamil,"my English mark",English,"my mas mark",mas,"my phy mark",phy,"my che mark",che,"my computer mark",computer,"my Totel mark",Totel)
